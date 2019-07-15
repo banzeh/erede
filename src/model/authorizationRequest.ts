70d0188fee970bd4f59c5120478b4b1b0fcefc41
Flavio Takeuchi <flavio@banzeh.com.br>
2019-07-15 16:36:41 -0300

    Campos opcionais na interface

--- src/model/authorizationRequest.ts
+++ src/model/authorizationRequest.ts
@@ -1,13 +1,13 @@
 export interface AuthorizationRequest {
   /** @description Define se a transação terá captura automática ou posterior. O não envio desse campo será considerado a captura automática (true) */
-  capture: boolean;
+  capture?: boolean;
   /** 
    * @description Tipo de transação a ser realizada.  
    * Para transações de crédito, utilize credit
    * Para transações de débito, utilize debit
    * @obs O não envio desse campo será considerado credit
   */
-  kind: string;
+  kind?: string;
   /** @description Código do pedido gerado pelo estabelecimento */
   reference: string;
   /** @description Valor total da transação sem separador de milhar e decimal.
@@ -18,7 +18,7 @@ export interface AuthorizationRequest {
    * @description Número de parcelas em que a transação será autorizada. De 2 a 12
    * O não envio desse campo será considerado à vista.
    */
-  installments: number;
+  installments?: number;
   /** @description Nome do portador impresso no cartão */ 
   cardHolderName: string;
   /** @description Número do cartão */
@@ -31,16 +31,16 @@ export interface AuthorizationRequest {
    * @description Código de segurança do cartão (geralmente localizado no verso do cartão)
    * O envio desse parâmetro garante maior possibilidade de aprovação da transação.
    */
-  securityCode: string;
+  securityCode?: string;
   /** @description Frase personalizada que será impressa na fatura do portador */
-  softDescriptor: string;
+  softDescriptor?: string;
   /**
    * @description Informa ao emissor se a transação é proveniente de uma recorrência. Se transação for uma recorrência, enviar true. 
    * Caso contrário, enviar false.
    * O não envio desse campo será considerado o valor false.
    * A Rede não gerencia os agendamentos de recorrência, apenas permite aos lojistas indicarem se a transação originada é de um plano recorrente.
    */
-  subscription: boolean;
+  subscription?: boolean;
   /**
    * @description Identifica a origem da transação
    * e.Rede - 1
@@ -48,7 +48,7 @@ export interface AuthorizationRequest {
    * VISA Checkout – 6
    * O não envio desse campo será considerado uma transação e.Rede (1)
    */
-  origin: number;
+  origin?: number;
   /** @description Número de filiação do distribuidor (PV) */
-  distributorAffiliation: number;
+  distributorAffiliation?: number;
 }
